73ceda7501d12e4a91cb85689b8f66260854e979
Roberto Salazar <roberto.salazar@upaep.mx>
2021-05-13 14:35:47 -0500

    Sesión 47 - 13/05/2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 // Rutas
 import { APP_ROUTING } from './app.routes';
@@ -15,13 +16,15 @@ import { APP_ROUTING } from './app.routes';
 import { HeroesService } from './Servicios/heroes.service';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
